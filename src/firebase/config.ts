601297ccee0683b0cc0c69fb8436dd7ab236a1bf
Crece Interno <creceinterno@gmail.com>
2025-10-06 18:20:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5484670460-98461",
+  "appId": "1:279349981589:web:9ad1f2fb88c97157cd869c",
+  "apiKey": "AIzaSyCVz2TfaM2JcDT_-TNxr8_P1Pf_VFFBxgM",
+  "authDomain": "studio-5484670460-98461.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "279349981589"
+};
