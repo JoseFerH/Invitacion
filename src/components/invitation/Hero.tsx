939b8cf3d297ac3de16e20092405c839678080af
Crece Interno <creceinterno@gmail.com>
2025-10-07 20:57:40 +0000

    Cambia la tipografía a Dulcinea (_for element h1_)

--- src/components/invitation/Hero.tsx
+++ src/components/invitation/Hero.tsx
@@ -11,7 +11,7 @@ export function Hero() {
         <p className="text-2xl md:text-2xl text-foreground/80 font-lato font-light">
             Te invitamos a celebrar la fiesta de graduación de
         </p>
-        <h1 className="text-9xl md:text-9xl font-great-vibes text-primary my-20">
+        <h1 className="text-9xl md:text-9xl font-dulcinea text-primary my-20">
             Gabriela
         </h1>
       </div>
